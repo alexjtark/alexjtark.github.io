53149a15e0796c7cd8cdd59631a133a17fdb4285
Alex Tarkowski <alex.tarkowski@gmail.com>
2016-10-07 14:11:07 -0400

    add new js events

--- assets/js/funevents.js
+++ assets/js/funevents.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
 	$('#nav').affix({
       offset: {
@@ -6,4 +7,11 @@ $(document).ready(function(){
 	});	
 
 	$('.lets-talk').hover(function(){$(this).addClass('animated swing');})
+
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top
+	    }, 500);
+	    return false;
+	});
 });
